41facd58ccfd4d8ada702222847ec1fac715f5fe
Thue <thuejk@gmail.com>
2012-07-14 19:50:01 +0200

    Show a "Loading..." text while loading action result.

--- fsroot/var/lib/minecraft/web_include/action.php
+++ fsroot/var/lib/minecraft/web_include/action.php
@@ -16,6 +16,9 @@ if (isset($_POST["download_map"])) {
 <body>
 <pre id="pre">
 </pre>
+<p id="loading_msg">
+Loading...
+</p>
 
 <script type="text/javascript">
 <?php
@@ -40,6 +43,10 @@ printf("query_string = '%s';", $qs);
       pre.innerHTML = "";
       pre.appendChild(text);
       }
+    if (xmlhttp.readyState==4) {
+      var loading_msg = document.getElementById("loading_msg");
+      loading_msg.parentNode.removeChild(loading_msg);
+    }
   }
   xmlhttp.open("POST","index.php?page=action_ajax",true);
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
