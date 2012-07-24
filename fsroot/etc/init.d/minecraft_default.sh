528c814aa1176ffdf5fe5509bf2db5027e9b2c18
Thue <thuejk@gmail.com>
2012-07-24 11:41:14 +0200

    Add LSB init script header ( http://wiki.debian.org/LSBInitScripts ). Fixes issue #3, which was that Debian complained that they were missing.

--- fsroot/etc/init.d/minecraft_default.sh
+++ fsroot/etc/init.d/minecraft_default.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+### BEGIN INIT INFO
+# Provides:          rfwadmin
+# Required-Start:    $remote_fs $network $time
+# Required-Stop:     $remote_fs $network
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: rfwadmin Minecraft server control script
+# Description:       rfwadmin runs any number of configured Minecraft servers on boot, which can then be controlled by the web interface and/or calling this init script command line.
+### END INIT INFO
+
 CMD="/var/lib/minecraft/servers/default/minecraft.sh"
 for ARG in "$@"; do
   CMD="$CMD \"$ARG\""
