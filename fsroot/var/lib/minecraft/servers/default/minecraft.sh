48ff5fe03fd7384399f05bfeb50b6749fa3851cc
Thue <thuejk@gmail.com>
2012-08-25 19:57:57 +0200

    Include minecraft server version 1.3.2, not 1.2.5.

--- fsroot/var/lib/minecraft/servers/default/minecraft.sh
+++ fsroot/var/lib/minecraft/servers/default/minecraft.sh
@@ -2,7 +2,7 @@
 
 #these two following lines (PATH_BASE and FILE_JAR) are regexp matched from serverjar.php . So don't make then too irregular.
 PATH_BASE="/var/lib/minecraft"
-FILE_JAR="$PATH_BASE/jars/serverjars/minecraft_server_1.2.5.jar"
+FILE_JAR="$PATH_BASE/jars/serverjars/minecraft_server_1.3.2.jar"
 
 #If running multiple servers, $SCREEN and $SERVER_SUBDIR should be unique
 #The SCREEN=... is inserted into a regexp, so may only contain [a-zA-Z0-9]
