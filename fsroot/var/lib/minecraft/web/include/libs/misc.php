3ebdb6ba204620de3d0b8221d69e614ea4fdd5d2
Thue <thuejk@gmail.com>
2013-05-10 15:55:17 +0200

    A function to generate a random string in php.

---
+++ fsroot/var/lib/minecraft/web/include/libs/misc.php
@@ -0,0 +1,15 @@
+class stdlib {
+  public static $PASSWORD_CHARS = "23456789abcdefghkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ";
+  public static function get_random_string($len) {
+    $clen = strlen(self::$PASSWORD_CHARS);
+
+    $s = "";
+    for ($i=0; $i<$len; $i++) {
+      $r = mt_rand(0, $clen-1);
+      $s .= self::$PASSWORD_CHARS{$r};
+    }
+
+    return $s;
+  }
+
+}
