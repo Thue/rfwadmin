d8c2bc0510683129bdc407ebdcc78f2121ef3194
Thue Janus Kristensen <thuejk@gmail.com>
2013-07-20 22:16:09 +0200

    Suggest setting timezone in index.php .

--- fsroot/var/www/index.php
+++ fsroot/var/www/index.php
@@ -11,6 +11,7 @@ require_once($include_base . "/web/include/libs/minecraft.php");
 $mc = new minecraft($include_base . "/servers/default" /* server dir */);
 //$mc->html_title = "Custom title here";
 //$mc->armory_enabled = true; //auto-download rfw maps from AuthorBlues autoref. Default false.
+//date_default_timezone_set("Asia/Pyongyang"); //various times displayed in files. Default `date +"%Z"`
 
 minecraft_map::$map_dir = $include_base . "/maps";
 plugins::$plugins_dir = $include_base . "/jars/plugins";
