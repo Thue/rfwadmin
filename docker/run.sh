2d5377fe399d2369f75cb38e6b6780f25e6584c2
Paulson McIntyre <paul@gpmidi.net>
2014-07-05 17:37:09 -0400

    Fixing docker-side build error: Directory '/var/run/screen' must have mode 777.

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -x
 
 echo "Starting at "`/bin/date`
 
+# Set screen dir perms if they aren't already set
+/bin/chmod 777 /var/run/screen
+/bin/chown root:root /var/run/screen
+
+# Ensure the standard files exist
 /bin/cp -a /var/lib/minecraft/servers/default/server.stock/* /var/lib/minecraft/servers/default/server/
+/bin/mkdir -p /var/log/httpd
+/bin/chown apache:apache /var/log/httpd
 
 /usr/sbin/apachectl -D FOREGROUND
 exit $?
