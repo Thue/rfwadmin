6e3aca984516a341d360961697de71225b0fc294
GpMidi <paul@gpmidi.net>
2014-07-08 16:30:38 -0400

    Trying to switch this to using ENV variables
    There is a better, cleaner way to do this. This is just a POC implementation.

--- fsroot/var/www/index.php
+++ fsroot/var/www/index.php
@@ -9,9 +9,15 @@ $include_base = "/var/lib/minecraft"; //grepped from install.sh
 $server_dir = $include_base . "/servers/default";
 require_once($include_base . "/web/include/libs/minecraft.php");
 $mc = new minecraft($include_base . "/servers/default" /* server dir */);
-//$mc->html_title = "Custom title here";
-//$mc->armory_enabled = true; //auto-download rfw maps from AuthorBlues autoref. Default false.
-//date_default_timezone_set("Asia/Pyongyang"); //various times displayed in files. Default `date +"%Z"`
+
+if (getenv("UI_HTML_TITLE"))
+  $mc->html_title = getenv("UI_HTML_TITLE");
+  
+if (getenv("UI_ARMORY_ENABLED")=="1")
+  $mc->armory_enabled = true; //auto-download rfw maps from AuthorBlues autoref. Default false.
+
+if (getenv("UI_PHP_TIMEZONE"))
+  date_default_timezone_set(getenv("UI_PHP_TIMEZONE")); //various times displayed in files. Default `date +"%Z"`
 
 minecraft_map::$map_dir = $include_base . "/maps";
 plugins::$plugins_dir = $include_base . "/jars/plugins";
