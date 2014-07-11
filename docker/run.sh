d4ef66500e4a23d8a6894129e8448e93766e965c
Paulson McIntyre <paul@gpmidi.net>
2014-07-11 12:35:07 -0400

    Trying some path changes

--- docker/run.sh
+++ docker/run.sh
@@ -3,14 +3,21 @@ set -x
 
 echo "Starting at "`/bin/date`
 
+cd /var/lib/minecraft/
+
 # Set screen dir perms if they aren't already set
 /bin/chmod 777 /var/run/screen
 /bin/chown root:root /var/run/screen
 
+
 # Ensure the standard files exist
-/bin/cp -a /var/lib/minecraft/servers/default/server.stock/* /var/lib/minecraft/servers/default/server/
-/bin/mkdir -p /var/log/httpd
-/bin/chown apache:apache /var/log/httpd
+/bin/cp -a /var/lib/minecraft/servers/default/server.stock/* \
+    /var/lib/minecraft/servers/default/server/
+/bin/mkdir -p /var/log/httpd /var/lib/minecraft/maps \
+    /var/lib/minecraft/servers /var/lib/minecraft/jars
+/bin/chown -R apache:apache /var/log/httpd /var/lib/minecraft/maps \
+    /var/lib/minecraft/servers /var/lib/minecraft/jars
+
 
 # Handle proxy settings
 if [ "$http_proxy" == "" ]; then
