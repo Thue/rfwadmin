29696e0611da8d6bcc2527cbdc49873f06b6285a
Paulson McIntyre <paul@gpmidi.net>
2014-07-11 12:06:51 -0400

    Trying to fix the env auto set

--- fsroot/var/www/index.php
+++ fsroot/var/www/index.php
@@ -10,13 +10,13 @@ $server_dir = $include_base . "/servers/default";
 require_once($include_base . "/web/include/libs/minecraft.php");
 $mc = new minecraft($include_base . "/servers/default" /* server dir */);
 
-if (getenv("UI_HTML_TITLE"))
+if (getenv("UI_HTML_TITLE")!=FALSE)
   $mc->html_title = getenv("UI_HTML_TITLE");
-  
+
 if (getenv("UI_ARMORY_ENABLED")=="1")
   $mc->armory_enabled = true; //auto-download rfw maps from AuthorBlues autoref. Default false.
 
-if (getenv("UI_PHP_TIMEZONE"))
+if (getenv("UI_PHP_TIMEZONE")!=FALSE)
   date_default_timezone_set(getenv("UI_PHP_TIMEZONE")); //various times displayed in files. Default `date +"%Z"`
 
 minecraft_map::$map_dir = $include_base . "/maps";
