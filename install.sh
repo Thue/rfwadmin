ecda9458f3129f99d24cd0551b6ac95b7358ae4e
Thue <thuejk@gmail.com>
2014-12-02 17:47:12 +0100

    Allow numbers in unix username in install.sh check.

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ if [ -z "$WEBSERVER_USER" ]; then
   WEBSERVER_USER=$(ps axho user,comm|grep -E "httpd|apache"|uniq|grep -v "root"|awk 'END {if ($1) print $1}')
   echo "Using '$WEBSERVER_USER' as the HTTP server user, which will also run the Minecraft server."
 fi
-if ! [[ "$WEBSERVER_USER" =~ ^[a-zA-Z_-]+$ ]] ; then
+if ! [[ "$WEBSERVER_USER" =~ ^[a-zA-Z_][0-9a-zA-Z_-]+$ ]] ; then
     error_exit "error: HTTP server user ($WEBSERVER_USER) looks wrong"
 fi
 
